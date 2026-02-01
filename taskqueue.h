050b16f19cb6ca723fe01ab00c3c5125d20fddaa
Kamil Barański <kamilbaranski@gmail.com>
2026-02-02 00:08:06 +0100

    +prevState and forward definitions

--- taskqueue.h
+++ taskqueue.h
@@ -3,17 +3,18 @@
 
 struct Task {
   MachineState state;
-  char motor; // 'W', 'T' lub 'S' (Synchronized)
+  MachineState prevState; // needed for pausing/resuming
+  char motor;  // 'W', 'T' lub 'S' (Synchronized)
   bool isRelative;
-  long targetPosition; // absolute
-  long targetSteps;    // relative
-  int dir;             // direction (-1 backward | 0 not set | 1 forward)
-  long currentSteps;   // steps counter
-  long accelDistance;  // how many steps it took to accelerate
+  long targetPosition;  // absolute
+  long targetSteps;     // relative
+  int dir;              // direction (-1 backward | 0 not set | 1 forward)
+  long currentSteps;    // steps counter
+  long accelDistance;   // how many steps it took to accelerate
   float startRPM;
   float targetRPM;
   float currentRPM;
-  float accelRate; // RPM zwiększane na krok Mastera
+  float accelRate;  // RPM zwiększane na krok Mastera
   bool isStarted;
   bool isDecelerating;
   bool isComplete;
@@ -21,8 +22,15 @@ struct Task {
 
 #define QUEUE_SIZE 6
 Task taskQueue[QUEUE_SIZE];
-int head = 0; // Index of the current task
-int tail = 0; // Index where next task will be added
+int head = 0;  // Index of the current task
+int tail = 0;  // Index where next task will be added
 int taskCount = 0;
+
+bool enqueueTask(MachineState s, char m, long target, bool isRelative, int rpm,
+                 float ramp);
+Task *getCurrentTask();
+void dequeueTask();
+void clearQueue();
+
 
 #endif
