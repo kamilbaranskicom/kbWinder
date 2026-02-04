0e32db2276248b405aadd987f5cd3ff013d1cd1d
Kamil Barański <kamilbaranski@gmail.com>
2026-02-04 23:00:39 +0100

    get memory, format

--- serial.ino
+++ serial.ino
@@ -3,7 +3,7 @@
 
 void processCommand(String cmd) {
   cmd.trim();
-  cmd.toUpperCase(); // so presets will be case-insensitive ;)
+  cmd.toUpperCase();  // so presets will be case-insensitive ;)
   Serial.println("Received: " + cmd);
 
   if (cmd.startsWith(F("STOP"))) {
@@ -28,7 +28,7 @@ void processCommand(String cmd) {
     absPos = 0;
     isHomed = true;
     Serial.println(
-        F("MSG: Machine absolute ZERO established at current position"));
+      F("MSG: Machine absolute ZERO established at current position"));
   } else if (cmd == F("SET HOME")) {
     // HOME w naszym nazewnictwie to startOffset w strukturze presetu
     active.startOffset = absPos;
@@ -40,14 +40,18 @@ void processCommand(String cmd) {
     handleSet(cmd.substring(4));
   } else if (cmd.startsWith(F("GET TASK"))) {
     printTaskDebug(getCurrentTask());
+  } else if (cmd.startsWith(F("GET MEMORY"))) {
+    printFreeMemory();
   } else if (cmd.startsWith(F("GET"))) {
     handleGet(cmd.substring(3));
-  } else if (cmd.startsWith(F("SAVE "))) {
-    savePreset(cmd.substring(5));
+  } else if (cmd.startsWith(F("SAVE"))) {
+    savePreset(cmd.substring(4));
   } else if (cmd.startsWith(F("LOAD "))) {
     loadPresetByName(cmd.substring(5));
   } else if (cmd.startsWith(F("DELETE "))) {
     deletePreset(cmd.substring(7));
+  } else if (cmd.startsWith(F("FORMAT"))) {
+    formatPresets();
   } else if (cmd.startsWith(F("EXPORT"))) {
     exportCSV();
   } else if (cmd.startsWith(F("HELP"))) {
@@ -61,7 +65,7 @@ void processCommand(String cmd) {
 
   } else if (cmd.startsWith(F("JOG "))) {
     moveManual(cmd.substring(4), true);
-  } // (... handle more commands...)
+  }  // (... handle more commands...)
   else if (cmd.startsWith("T ") || cmd.startsWith("W ")) {
     moveManual(cmd);
   }
@@ -69,73 +73,73 @@ void processCommand(String cmd) {
 
 void printHelp() {
   Serial.println(
-      F("Movement: W, T, GOTO, SEEK ZERO\n"
-        "Control: START, STOP, PAUSE, RESUME\n"
-        "Presets: SAVE, LOAD, DELETE, EXPORT, IMPORT\n"
-        "Settings: GET MACHINE/PRESET/RUNTIME/..., SET ..., FACTORY\n"
-        "Info: STATUS, HELP, LONGHELP, SETHELP"));
+    F("Movement: W [revs] [speed], T [dist] [speed],\n"
+      "          GOTO [ZERO|BACKOFF|START|<absPos>], SEEK ZERO\n"
+      "Control: START [values], STOP, PAUSE, RESUME\n"
+      "Presets: SAVE [name], LOAD [name], DELETE [name], FORMAT, EXPORT\n"
+      "Settings: GET [MACHINE|PRESET|RUNTIME|MEMORY|<val>], SET ..., FACTORY\n"
+      "Info: STATUS, HELP, LONGHELP, SETHELP"));
 }
 
 void printLongHelp() {
   Serial.println(
-      F("START (<preset>|<wire-diameter> <coil-length> <turns> [rpm] [ramp] "
-        "[offset]):\n"
-        "  starts winding the coil\n"
-        "STOP: stop winding\n"
-        "PAUSE: pause winding and put motors in offline; doesn't reset "
-        "position\n"
-        "RESUME: resume winding after PAUSE\n"
-        "SEEK ZERO [speed]: finds ZERO position (by moving Traverse backward\n"
-        "  until the limit switch is found or STOP command is sent)\n"
-        "W <distance> [speed]: move Winder to relative position (in turns)\n"
-        "T <distance> [speed]: move Traverse to relative position (in mm)\n"
-        "JOG W/T <distance> [speed]: move to relative position, but stops\n"
-        "  if no JOG PING received within 2 secs\n"
-        "JOG PING: ping for JOG W/T\n"
-        "GOTO <position> [speed]: move Traverse to absolute position\n"
-        "GOTO (ZERO|START|BACKOFF) [speed]: move Traverse to (zero|\n"
-        "  preset start|backoff distance) position\n"
-        "SAVE <name> [<wire-diameter> <coil-length> <turns>]: saves preset "
-        "<name>\n"
-        "LOAD <name>: loads preset <name>\n"
-        "DELETE <name>: deletes preset <name>\n"
-        "EXPORT: prints presets in CSV format\n"
-        "IMPORT: imports presets in CSV format (ends with empty line)\n"
-        "STATUS: prints status\n"
-        "FACTORY: loads default machine settings\n"
-        "SET ... : sets parameter(s)\n"
-        "GET ... : gets parameter(s)\n"
-        "SETHELP: parameters list\n"
-        "HELP: short help\n"
-        "LONGHELP: this help"));
+    F("START (<preset>|<wire-diameter> <coil-length> <turns> [rpm] [ramp] "
+      "[offset]):\n"
+      "  starts winding the coil\n"
+      "STOP: stop winding\n"
+      "PAUSE: pause winding and put motors in offline; doesn't reset "
+      "position\n"
+      "RESUME: resume winding after PAUSE\n"
+      "SEEK ZERO [speed]: finds ZERO position (by moving Traverse backward\n"
+      "  until the limit switch is found or STOP command is sent)\n"
+      "W <distance> [speed]: move Winder to relative position (in turns)\n"
+      "T <distance> [speed]: move Traverse to relative position (in mm)\n"
+      "JOG W/T <distance> [speed]: move to relative position, but stops\n"
+      "  if no JOG PING received within 2 secs\n"
+      "JOG PING: ping for JOG W/T\n"
+      "GOTO <position> [speed]: move Traverse to absolute position\n"
+      "GOTO (ZERO|START|BACKOFF) [speed]: move Traverse to (zero|\n"
+      "  preset start|backoff distance) position\n"
+      "SAVE [<name>|<csvString>]: saves preset\n"
+      "LOAD <name>: loads preset <name>\n"
+      "DELETE <name>: deletes preset <name>\n"
+      "FORMAT: deletes all presets\n"
+      "EXPORT: prints presets in CSV format\n"
+      "STATUS: prints status\n"
+      "FACTORY: loads default machine settings\n"
+      "SET ... : sets parameter(s)\n"
+      "GET ... : gets parameter(s)\n"
+      "SETHELP: parameters list\n"
+      "HELP: short help\n"
+      "LONGHELP: this help"));
 }
 
 void printSetHelp() {
   // todo - it's a bit obsolete
   Serial.println(
-      F("SET <wire-diameter> <coil-length> <turns>: sets parameters\n"
-        "SET WIRE <wire-diameter>: in mm\n"
-        "SET COIL LENGTH <coil-length>: in mm\n"
-        "SET TURNS <turns>: how many turns the winder should go\n"
-        "SET ZERO: sets ZERO position to current position\n"
-        "SET HOME: sets HOME position to current position\n"
-        "SET SCREW PITCH <pitch>: sets screw pitch in mm\n"
-        "SET WINDER STEPS <steps>: sets winder steps\n"
-        "SET TRAVERSE STEPS <steps>: sets traverse steps\n"
-        "SET WINDER SPEED <rpm>: sets winder max speed\n"
-        "SET TRAVERSE SPEED <rpm>: sets traverse max speed\n"
-        "SET WINDER DIR [FORWARD|BACKWARD]: sets winder direction\n"
-        "SET TRAVERSE DIR [FORWARD|BACKWARD]: sets traverse direction\n"
-        "SET LIMIT SWITCH [ON|OFF]: if off, seeks for ZERO slower (to allow\n"
-        "  for manual STOP command)\n"
-        "SET ZERO BEFORE HOME [ON|OFF]: if on, finds zero when going HOME\n"
-        "SET HOME BEFORE START [ON|OFF]: if on, goes HOME before winding.\n"
-        "GET [parameter]: prints current value of <parameter> (or all "
-        "parameters if not specified)"));
+    F("SET <wire-diameter> <coil-length> <turns>: sets parameters\n"
+      "SET WIRE <wire-diameter>: in mm\n"
+      "SET COIL LENGTH <coil-length>: in mm\n"
+      "SET TURNS <turns>: how many turns the winder should go\n"
+      "SET ZERO: sets ZERO position to current position\n"
+      "SET HOME: sets HOME position to current position\n"
+      "SET SCREW PITCH <pitch>: sets screw pitch in mm\n"
+      "SET WINDER STEPS <steps>: sets winder steps\n"
+      "SET TRAVERSE STEPS <steps>: sets traverse steps\n"
+      "SET WINDER SPEED <rpm>: sets winder max speed\n"
+      "SET TRAVERSE SPEED <rpm>: sets traverse max speed\n"
+      "SET WINDER DIR [FORWARD|BACKWARD]: sets winder direction\n"
+      "SET TRAVERSE DIR [FORWARD|BACKWARD]: sets traverse direction\n"
+      "SET LIMIT SWITCH [ON|OFF]: if off, seeks for ZERO slower (to allow\n"
+      "  for manual STOP command)\n"
+      "SET ZERO BEFORE HOME [ON|OFF]: if on, finds zero when going HOME\n"
+      "SET HOME BEFORE START [ON|OFF]: if on, goes HOME before winding.\n"
+      "GET [parameter]: prints current value of <parameter> (or all "
+      "parameters if not specified)"));
 }
 
 void printStatus() {
-  Task *t = getCurrentTask();
+  Task* t = getCurrentTask();
   Serial.println(F("--- MACHINE STATUS ---"));
   if (t == NULL) {
     Serial.println(F("State: IDLE"));
@@ -167,4 +171,40 @@ void printStatus() {
   Serial.println(F("----------------------"));
 
   return;
+}
+
+
+#ifdef __arm__
+// Dla procesorów ARM (np. Due)
+extern "C" char* sbrk(int incr);
+int freeMemory() {
+  char top;
+  return &top - reinterpret_cast<char*>(sbrk(0));
+}
+#elif defined(AVR)
+// Dla Arduino Nano/Uno (AVR)
+extern unsigned int __heap_start;
+extern void *__brkval;
+
+int freeMemory() {
+  int free_memory;
+  if ((int)__brkval == 0)
+    free_memory = ((int)&free_memory) - ((int)&__heap_start);
+  else
+    free_memory = ((int)&free_memory) - ((int)__brkval);
+
+  return free_memory;
+}
+#endif
+
+
+void printFreeMemory() {
+  int freeRAM = freeMemory();
+  Serial.print(F("SYSTEM: Free RAM: "));
+  Serial.print(freeRAM);
+  Serial.println(F(" bytes"));
+
+  if (freeRAM < 200) {
+    Serial.println(F("WARNING: Memory critical! Fragmentation possible."));
+  }
 }
